fd4f448676c99cb39e79adfc9684354648377e55
kulshav <vovakulsha99@gmail.com>
2024-05-16 16:30:49 +0500

    Small changes

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
+from configs.settings import data_settings as dts
+from preprocessing.docx_loader import get_documents
+from preprocessing.tokenizer import tokenize_text
 
-
-
+if __name__ == "__main__":
+    text = get_documents()[0]
+    sentence_tokens = tokenize_text(text=text, token_type=dts.sentence_tokenizer)
+    print(sentence_tokens)
